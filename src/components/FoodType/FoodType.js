c526d6c3077d65be965bfbdd8d7ae14b769e84c5
Severin Hanevik <severin.hanevik@gmail.com>
2022-05-09 21:18:07 +0200

    🔨Use css module for FoodType

--- src/components/FoodType/FoodType.js
+++ src/components/FoodType/FoodType.js
@@ -1,12 +1,12 @@
 import React from "react"
-import "./FoodType.css"
+import styles from "./FoodType.module.css"
 
 const FoodType = props => {
   const { name, calories, weight } = props.def
   return (
-    <div>
-      <ul>
-        <h1 style={{ fontSize: 22 }}>{name}</h1>
+    <div className={styles.div}>
+      <ul className={styles.ul}>
+        <h1 className={styles.h1}>{name}</h1>
         <li>Number of calories: {calories}</li>
         <li>Weight: {weight}</li>
       </ul>
