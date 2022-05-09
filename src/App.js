3a912bb1c671a8dcd50a0dd3b541fddfa0ecfec2
Severin Hanevik <severin.hanevik@gmail.com>
2022-05-09 09:05:40 +0200

    🎨Add basic css to FoodType

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css"
 import FoodType from "./components/FoodType/FoodType"
 
+const bpp = {
+  name: "Billy's Pan Pizza",
+  calories: 450,
+  weight: 170,
+}
+
 function App() {
   return (
     <div className="App">
-      <FoodType
-        foodName={"Billy's Pan Pizza"}
-        foodCals={450}
-        foodWeight={170}
-      />
+      <FoodType def={bpp} />
     </div>
   )
 }
--- src/components/FoodType/FoodType.js
+++ src/components/FoodType/FoodType.js
@@ -1,12 +1,14 @@
 import React from "react"
+import "./FoodType.css"
 
-const FoodType = ({ foodName, foodCals, foodWeight }) => {
+const FoodType = props => {
+  const { name, calories, weight } = props.def
   return (
     <div>
       <ul>
-        <li>{foodName}</li>
-        <li>{foodCals}</li>
-        <li>{foodWeight}</li>
+        <h1 style={{ fontSize: 22 }}>{name}</h1>
+        <li>Number of calories: {calories}</li>
+        <li>Weight: {weight}</li>
       </ul>
     </div>
   )
