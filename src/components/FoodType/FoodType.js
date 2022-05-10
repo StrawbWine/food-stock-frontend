cd9d5a1d5b2320c5de8fdbb792d3714f02944584
Severin Hanevik <severin.hanevik@gmail.com>
2022-05-10 20:50:21 +0200

    ✨Add counter and buttons

--- src/components/FoodType/FoodType.js
+++ src/components/FoodType/FoodType.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./FoodType.module.css"
 
 const FoodType = props => {
   const { name, calories, weight } = props.def
+
+  const [count, setCount] = useState(0)
+
   return (
     <div className={styles.div}>
       <ul className={styles.ul}>
@@ -10,6 +13,23 @@ const FoodType = props => {
         <li>Number of calories: {calories}</li>
         <li>Weight: {weight}</li>
       </ul>
+      <div className={styles.buttonPanel}>
+        <button
+          className={styles.plusButton}
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          className={styles.minusButton}
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <div className={styles.countBox}>
+          <span class={styles.count}>{count}</span>
+        </div>
+      </div>
     </div>
   )
 }
