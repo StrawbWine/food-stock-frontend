cde7dbd3f8c408f59ea83ffd63556898b4f43789
Severin Hanevik <severin.hanevik@gmail.com>
2022-05-14 18:10:37 +0200

    🎨Improve food form css

--- src/components/AddFoodTypeForm/AddFoodTypeForm.js
+++ src/components/AddFoodTypeForm/AddFoodTypeForm.js
@@ -1,18 +1,25 @@
 import React from "react"
 import styles from "./AddFoodTypeForm.module.css"
 
-const AddFoodTypeForm = ({handleFoodSubmit}) => {  
-  return <div className={styles.frame}>
-    <form className={styles.form} onSubmit={handleFoodSubmit}>
-      <label>Name of food type</label>
-      <input type="text" id="foodName" placeholder="Banana, pizza" />
-      <label>Number of calories</label>
-      <input type="text" id="calories" placeholder="0" />
-      <label>Weight</label>
-      <input type="text" id ="weight" placeholder="0" />
-      <input type="submit" value="Add new food type" />
-    </form>
-  </div>
+const AddFoodTypeForm = ({ handleFoodSubmit }) => {
+  return (
+    <div className={styles.frame}>
+      <form className={styles.form} onSubmit={handleFoodSubmit}>
+        <h3>Add new food type</h3>
+        <label>Name of food type</label>
+        <input type="text" id="foodName" placeholder="Banana, pizza" />
+        <label>Number of calories</label>
+        <input type="text" id="calories" placeholder="0" />
+        <label>Weight</label>
+        <input type="text" id="weight" placeholder="0" />
+        <input
+          type="submit"
+          value="Add new food type"
+          className={styles.submit}
+        />
+      </form>
+    </div>
+  )
 }
 
 export default AddFoodTypeForm
