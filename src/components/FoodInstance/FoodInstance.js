7a813bea7fdfad7909813a2ca4629bdaf0e51073
StrawbWine <severin.hanevik@gmail.com>
2022-05-25 21:05:05 +0200

    🔨Change to fit API

--- src/components/FoodInstance/FoodInstance.js
+++ src/components/FoodInstance/FoodInstance.js
@@ -1,10 +1,10 @@
 import styles from "./FoodInstance.module.css"
 
 const FoodInstance = props => {
-  const { foodType, purchaseDate, expirationDate } = props.data
+  const { foodType, purchaseDate, expirationDate } = props.data  
   return (
     <div className={styles.foodInstance}>
-      <h3 className={styles.title}>{foodType}</h3>
+      <h3 className={styles.title}>{foodType.name}</h3>
       <ul className={styles.list}>
         <li>Date purchased: {purchaseDate}</li>
         <li>Expiration date: {expirationDate}</li>        
