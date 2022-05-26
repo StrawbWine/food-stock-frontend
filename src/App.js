88ffbd5cd8afd88e95f97def40f0999d827aac93
StrawbWine <54211990+StrawbWine@users.noreply.github.com>
2022-05-26 10:34:04 +0200

    ✨Show food instances (#2)
    * ✨Simple food instance setup with test data
    
    * ✨Fetch foodinstances from real API
    
    * 🔨Change to fit API
    
    * 🎨Most basic css on foodinstance components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useState } from "react"
+import axios from "axios"
+
 import "./App.css"
 import AddFoodTypeForm from "./components/AddFoodTypeForm/AddFoodTypeForm"
 import FoodContainer from "./components/FoodContainer/FoodContainer"
+import FoodInstanceContainer from "./components/FoodInstanceContainer/FoodInstanceContainer"
 import Header from "./components/Header/Header"
 import useApi from "./hooks/useApi"
 
@@ -8,12 +12,38 @@ function App() {
   const API_URL = process.env.REACT_APP_API_URL
 
   const [foodTypes, getFoodTypes] = useApi(`${API_URL}/foodtypes`)
+  const [foodInstanceData, setFoodInstanceData] = useState(null)
+
+  // const testInstances = [
+  //   {
+  //     id: 1,
+  //     foodType: "Pizza",
+  //     purchaseDate: "2022-05-10",
+  //     expirationDate: "2023-05-10"
+  //   },
+  //   {
+  //     id: 2,
+  //     foodType: "Jarlsberg",
+  //     purchaseDate: "2022-05-10",
+  //     expirationDate: "2023-05-10"
+  //   },
+  // ]
+
+  const handleFoodTypeClick = async foodTypeName => {    
+    try {      
+      const result = await axios.get(`${API_URL}/foodinstances?food_type_name=${foodTypeName}`)   
+      setFoodInstanceData(result.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className="App">
       <Header />
-      <FoodContainer foodTypes={foodTypes} getFoodTypes={getFoodTypes} />
+      <FoodContainer foodTypes={foodTypes} getFoodTypes={getFoodTypes} handleFoodTypeClick={handleFoodTypeClick} />
       <AddFoodTypeForm getFoodTypes={getFoodTypes} />
+      {foodInstanceData && <FoodInstanceContainer foodInstances={foodInstanceData} />}
     </div>
   )
 }
