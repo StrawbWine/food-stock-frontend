32e3fdbc490869dfa48c63d930d827aae89c059e
StrawbWine <severin.hanevik@gmail.com>
2022-05-27 23:40:28 +0200

    🎨Add icons to FoodInfo component

--- src/components/FoodInfo/FoodInfo.js
+++ src/components/FoodInfo/FoodInfo.js
@@ -1,13 +1,25 @@
 import styles from "./FoodInfo.module.css"
+import caloriesImg from "../../assets/overweight_filled.png"
+import weightImg from "../../assets/weight.png"
+import descriptionImg from "../../assets/i_note_action.png"
 
 const FoodInfo = props => {
   const { name, description, kcalPerHundredGrams, weightInGrams } = props.data
   return (
     <ul className={styles.ul}>
       <h1 className={styles.h1}>{name}</h1>
-      <li>Description: {description}</li>
-      <li>Number of calories: {kcalPerHundredGrams}</li>
-      <li>Weight: {weightInGrams}</li>
+      <li>
+        <img title="Description" src={descriptionImg}  />
+        {description}
+      </li>
+      <li>
+        <img title="Calories per 100 grams" src={caloriesImg} />
+        {kcalPerHundredGrams}
+      </li>
+      <li>
+        <img title="Weight in grams" src={weightImg} />
+        {weightInGrams}
+      </li>
     </ul>
   )
 }
